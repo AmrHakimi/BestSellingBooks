477fa457d593c02f3ffa0a726c9127a9509b890d
AmrHakimi <161561072+AmrHakimi@users.noreply.github.com>
2024-02-29 14:51:54 +0800

    Update BestSellingBooks.py

--- BestSellingBooks.py
+++ BestSellingBooks.py
@@ -11,12 +11,12 @@ This app predicts the **Best Selling Books** type!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    'Authors' = st.sidebar.selectbox('Authors', )
-    'Original language' = st.sidebar.selectbox('Original language',  3=Chinese,  4=Czech,  0=Dutch,  7=English, 11=French,
+    Authors = st.sidebar.selectbox('Authors', )
+    Original language = st.sidebar.selectbox('Original language',  3=Chinese,  4=Czech,  0=Dutch,  7=English, 11=French,
                                               13=German,  5=Gujarati,  8=Hindi, 10=Italian, 12=Japanese,  2=Norwegian, 14=Portuguese, 
                                               9=Russian,  1=Spanish, 15=Swedish,  6=Yiddish)
-    'First published' = st.sidebar.slider('First published', 0.3, 114, 20)
-    'Genre' = st.sidebar.slider('Genre', 0.3, 114, 20)
+    First published = st.sidebar.slider('First published', 0.3, 114, 20)
+    Genre = st.sidebar.slider('Genre', 0.3, 114, 20)
     data = {'Authors': Authors,
             'Original language': Original language,
             'First published': First published
