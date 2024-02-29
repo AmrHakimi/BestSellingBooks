9163119ae6e9d9499f9a4ed7711be88b7458a42f
AmrHakimi <161561072+AmrHakimi@users.noreply.github.com>
2024-02-29 13:11:59 +0800

    Update BestSellingBooks.py

--- BestSellingBooks.py
+++ BestSellingBooks.py
@@ -11,12 +11,14 @@ This app predicts the **Best Selling Books** type!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV = st.sidebar.slider('TV', 0.7, 297, 100)
-    Radio = st.sidebar.slider('Radio', 0, 50, 15)
-    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114, 20)
-    data = {'TV': TV,
-            'Radio': Radio,
-            'Newspaper': Newspaper}
+    Author(s) = st.sidebar.slider('Author(s)', 0.7, 297, 100)
+    Original language = st.sidebar.slider('Original language', 0, 50, 15)
+    First published = st.sidebar.slider('First published', 0.3, 114, 20)
+    Genre = st.sidebar.slider('Genre', 0.3, 114, 20)
+    data = {'Author(s)': Author(s),
+            'Original language': Original language,
+            'First published': First published
+            'Genre': Genre}
     features = pd.DataFrame(data, index=[0])
     return features
 
@@ -25,7 +27,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-loaded_model = pickle.load(open("", "rb"))
+loaded_model = pickle.load(open("Best_Selling_Bookss.h5", "rb"))
 
 prediction = loaded_model.predict(df)
 
