5fd9e5ff6e476462b7cc6ecf15cdf8e8c687a461
AmrHakimi <161561072+AmrHakimi@users.noreply.github.com>
2024-02-29 12:18:11 +0800

    Update BestSellingBooks.py

--- BestSellingBooks.py
+++ BestSellingBooks.py
@@ -1,9 +1,33 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
+import pickle
 
-st.header("My first Streamlit App")
-st.write(pd.DataFrame({
-    'Intplan': ['yes', 'yes', 'yes', 'no'],
-    'Churn Status': [0, 0, 0, 1]
-}))
+st.write("""
+# Best Selling Books App
+
+This app predicts the **Best Selling Books** type!
+""")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    TV = st.sidebar.slider('TV', 0.7, 297, 100)
+    Radio = st.sidebar.slider('Radio', 0, 50, 15)
+    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114, 20)
+    data = {'TV': TV,
+            'Radio': Radio,
+            'Newspaper': Newspaper}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+loaded_model = pickle.load(open("", "rb"))
+
+prediction = loaded_model.predict(df)
+
+st.subheader('Prediction')
+st.write(prediction)
